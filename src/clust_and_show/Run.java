e271d32ad865da194f258e2a7fc793acdc6f13a9
wangke1996 <porkw@outlook.com>
2017-04-24 16:35:26 +0800

    mainly update: add a new function hypernym_sm, using the slave_master relationships to help hypernym extraction.

--- src/clust_and_show/Run.java
+++ src/clust_and_show/Run.java
@@ -3,20 +3,32 @@ package clust_and_show;
 import java.awt.BorderLayout;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.Vector;
+
+import feature_abstract.Feature;
 
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextPane;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.MutableTreeNode;
+import javax.swing.tree.TreeNode;
 
 import java.util.Map.Entry;
 
@@ -24,9 +36,10 @@ import java.util.Map.Entry;
 
 public class Run {
 	//protected static String[] all_features={"look", "screen", "price", "camera", "time", "speed", "battery", "version", "quality", "performance", "storage", "user", "feature", "purchase", "design", "value", "part", "size"};
-	//protected static String folder="E:/Tsinghua/±œ…Ë/project1/remote_service";
-	protected static String folder=".";
+	protected static String folder="E:/Tsinghua/±œ…Ë/project1/remote_service/";
+	//protected static String folder="./";
 	protected static List<String> all_features=new ArrayList<String>();
+	protected static HashMap<String,Feature> FEATURE=new HashMap<String,Feature>();
 	protected static int feature_num=53;	
 	protected static int dim=54;
 	protected static Integer cid=0;
@@ -36,7 +49,66 @@ public class Run {
 	protected static HashMap<Integer,String> clust_labels=new HashMap<Integer,String>();
 	protected static List<HashSet<String>> clusters=new ArrayList<HashSet<String>>();
 	protected static HashMap<Integer,HashSet<String>> cid_feature=new HashMap<Integer,HashSet<String>>();
-	
+	protected static HashMap<String,Double> IZscore=new HashMap<String,Double>();
+	protected static HashMap<String,Double> EZscore=new HashMap<String,Double>();
+	protected static HashMap<String,Double> InSim=new HashMap<String,Double>();
+	protected static HashMap<String,Double> ExSim=new HashMap<String,Double>();
+	protected static DefaultMutableTreeNode tree_prun(DefaultMutableTreeNode root){
+		if(root.isLeaf())
+			return root;
+		boolean flag_change=false;
+		do{
+			flag_change=false;
+			if(root.getChildCount()==1){
+				root=(DefaultMutableTreeNode) root.getChildAt(0);
+				flag_change=true;
+			}
+			for(int k=0;k<root.getChildCount();k++){
+				DefaultMutableTreeNode child_node=(DefaultMutableTreeNode) root.getChildAt(k);
+				root.remove(k);
+				root.add(tree_prun(child_node));
+			}
+		}
+		while(flag_change);
+		return root;
+	}
+	protected static DefaultMutableTreeNode whole_prun(DefaultMutableTreeNode root){
+		HashSet<TreeNode> nodes=new HashSet<TreeNode>();
+		Enumeration<TreeNode> enu=root.postorderEnumeration();
+		while(enu.hasMoreElements()){
+			TreeNode t=enu.nextElement();
+			if(t.isLeaf())
+				continue;
+			if(has_leaf_child(t))
+			{
+				boolean flag=true;
+				TreeNode p=t.getParent();
+				while(p!=null){
+					if(has_leaf_child(p)){
+						flag=false;
+						break;
+					}
+					p=p.getParent();
+				}
+				if(flag){
+					nodes.add(t);
+				}
+			}
+		}
+		root.removeAllChildren();
+		Iterator<TreeNode> iter=nodes.iterator();
+		while(iter.hasNext())
+			root.add((MutableTreeNode) iter.next());
+		return root;
+	}
+	protected static boolean has_leaf_child(TreeNode root){
+		Enumeration<TreeNode> enu=root.children();
+		while(enu.hasMoreElements()){
+			if(enu.nextElement().isLeaf())
+				return true;
+		}
+		return false;
+	}
 	protected static DefaultMutableTreeNode ROOT=new DefaultMutableTreeNode("phone");
 	public static void main(String[] args){
 //		//String method="direct";//"graph";"agglo";"rbr";"bagglo";"br";
@@ -129,9 +201,11 @@ public class Run {
 		if(args.length>1)
 			dim=Integer.parseInt(args[1]);
 		bisection();
+		ROOT=tree_prun(ROOT);
+		ROOT=whole_prun(ROOT);
         JFrame f = new JFrame("JTreeDemo");
         JTree T=new JTree(ROOT);
-        //f.add(T);
+        f.add(T);
         JScrollPane scrollPane_2 = new JScrollPane();
         f.getContentPane().add(scrollPane_2, BorderLayout.CENTER);
         f.setSize(900, 900);
@@ -142,7 +216,7 @@ public class Run {
     }
 	public static void bisection(){
 		try{
-			FileReader fr=new FileReader(folder+"/context_vecs.txt");
+			FileReader fr=new FileReader(folder+"context_vecs.txt");
 			BufferedReader br=new BufferedReader(fr);
 			br.readLine();
 			String s;
@@ -160,20 +234,20 @@ public class Run {
 			e.printStackTrace();
 		}
 		read_features();
-		bis_split(all_features,ROOT);
+		bis_split(all_features,ROOT,"phone");
 		get_hypernym();
 		String result="";
 		Iterator<Entry<Integer,HashSet<String>>> iter=cid_feature.entrySet().iterator();
 		while(iter.hasNext()){
 			Entry<Integer,HashSet<String>> ent=iter.next();
-			result=result+"cid = "+ent.getKey().toString()+" hypernym = "+clust_labels.get(ent.getKey())+"\n";
+			result=result+"cid = "+ent.getKey().toString()+" hypernym = "+clust_labels.get(ent.getKey())+" ISim = "+compute_ISim(new ArrayList<String>(ent.getValue()))+"\n";
 			Iterator<String> it=ent.getValue().iterator();
 			while(it.hasNext())
 				result=result+it.next()+' ';
 			result+='\n';
 		}
 		try{
-			FileWriter fw=new FileWriter(folder+"/result_direct_cos_bisc.txt");
+			FileWriter fw=new FileWriter(folder+"result_direct_cos_bisc.txt");
 			BufferedWriter bw=new BufferedWriter(fw);
 			bw.write(result);
 			bw.close();
@@ -183,20 +257,28 @@ public class Run {
 			e.printStackTrace();
 		}
 	}
-	public static void bis_split(List<String> features,DefaultMutableTreeNode root){
+	public static void bis_split(List<String> features,DefaultMutableTreeNode root,String hyper){
 		if(compute_ISim(features)>=th||features.size()<2){
-			for(String f:features)
+			HashSet<String> feature_set=new HashSet<String>(features);
+			prun_clust(feature_set ,hyper);
+			if(feature_set.isEmpty())
+				root.removeFromParent();
+			if(feature_set.size()==1){
+				root.setUserObject(feature_set.iterator().next());
+				return;
+			}
+			for(String f:feature_set)
 				root.add(new DefaultMutableTreeNode(f));
 			return;
 		}
 		write_input_matrix(features);
-		String command=folder+"/vcluster.exe -clmethod=direct -sim=cos -rowmodel=maxtf "+folder+"/Input_Matrix_"+cid.toString()+".txt 2";
+		String command=folder+"vcluster.exe -clmethod=direct -sim=cos -rowmodel=maxtf "+folder+"Input_Matrix_"+cid.toString()+".txt 2";
 		CommandUtil util = new CommandUtil();
         util.executeCommand(command);
         printList(util.getStdoutList());
         System.out.println("--------------------");
         printList(util.getErroroutList());
-        String outfile=folder+"/Input_Matrix_"+cid.toString()+".txt.clustering.2";
+        String outfile=folder+"Input_Matrix_"+cid.toString()+".txt.clustering.2";
         HashSet<String> hs1=new HashSet<String>();
         HashSet<String> hs2=new HashSet<String>();
         cid_feature.put(cid, hs1);
@@ -222,23 +304,22 @@ public class Run {
         catch(Exception e){
         	e.printStackTrace();
         }
-        DefaultMutableTreeNode node1=new DefaultMutableTreeNode(hypernym(cid_feature.get(cid)));
-        DefaultMutableTreeNode node2=new DefaultMutableTreeNode(hypernym(cid_feature.get(cid+1)));
+//        String hyper1=hypernym_zscore(cid_feature.get(cid));
+//        String hyper1=hypernym_overlap(cid_feature.get(cid));
+        String hyper1=hypernym_sm(cid_feature.get(cid));
+        DefaultMutableTreeNode node1=new DefaultMutableTreeNode(hyper1);
+//        String hyper2=hypernym_zscore(cid_feature.get(cid));
+//        String hyper2=hypernym_overlap(cid_feature.get(cid+1));
+        String hyper2=hypernym_sm(cid_feature.get(cid+1));
+        DefaultMutableTreeNode node2=new DefaultMutableTreeNode(hyper2);
         root.add(node1);
         root.add(node2);
-        List<String> features1=new ArrayList<String>();//[cid_feature.get(cid).size()];
-        List<String> features2=new ArrayList<String>();//String[cid_feature.get(cid+1).size()];
-        Iterator<String> it=cid_feature.get(cid).iterator();
-        while(it.hasNext()){
-        	features1.add(it.next());
-        }
-        it=cid_feature.get(cid+1).iterator();
-        while(it.hasNext()){
-        	features2.add(it.next());
-        }
+        List<String> features1=new ArrayList<String>(cid_feature.get(cid));//[cid_feature.get(cid).size()];
+        List<String> features2=new ArrayList<String>(cid_feature.get(cid+1));//String[cid_feature.get(cid+1).size()];
+     
         cid+=2;
-        bis_split(features1,node1);
-        bis_split(features2,node2);
+        bis_split(features1,node1,hyper1);
+        bis_split(features2,node2,hyper2);
 	}
 	public static int get_id(String f){
 		for(int m=0;m<all_features.size();m++){
@@ -249,7 +330,7 @@ public class Run {
 	}
 	public static void write_input_matrix(List<String> features){
 		try{
-			FileWriter fw=new FileWriter(folder+"/Input_Matrix_"+cid.toString()+".txt");
+			FileWriter fw=new FileWriter(folder+"Input_Matrix_"+cid.toString()+".txt");
 			BufferedWriter bufw=new BufferedWriter(fw);
 			bufw.write(features.size()+" "+dim+'\n');		
 			for(int k=0;k<features.size();k++){
@@ -296,12 +377,45 @@ public class Run {
 		Iterator<Entry<Integer,HashSet<String>>> iter=cid_feature.entrySet().iterator();
 		while(iter.hasNext()){
 			Entry<Integer,HashSet<String>> ent=iter.next();
-			clust_labels.put(ent.getKey(), hypernym(ent.getValue()));
+//			clust_labels.put(ent.getKey(), hypernym_zscore(ent.getValue()));
+//			clust_labels.put(ent.getKey(), hypernym_overlap(ent.getValue()));
+			clust_labels.put(ent.getKey(), hypernym_sm(ent.getValue()));
 		}
 	}
-	public static String hypernym(HashSet<String> Features){
+	
+	public static String hypernym_sm(HashSet<String> features){
+		
+		HashMap<String,Integer> master_candidate=new HashMap<String,Integer>();
+		for(String f:features){
+			Feature feature=FEATURE.get(f);
+			Iterator<Entry<String,Integer>> iter=feature.master_feature_union.entrySet().iterator();
+			while(iter.hasNext()){
+				Entry<String,Integer> ent=iter.next();
+				String master=ent.getKey();
+				if(!master_candidate.containsKey(master))
+					master_candidate.put(master, 0);
+				Integer freq=master_candidate.get(master);
+				freq+=ent.getValue();//*feature.freq;
+				master_candidate.put(master, freq);
+			}
+		}
+		List<HashMap.Entry<String, Integer>> masters =
+			    new ArrayList<HashMap.Entry<String, Integer>>(master_candidate.entrySet());
+		Collections.sort(masters, new Comparator<Map.Entry<String, Integer>>() { 
+			public int compare(Map.Entry<String, Integer> o1, Map.Entry<String,Integer> o2) {      
+				return (o2.getValue() - o1.getValue()); 
+				//return (o1.getKey()).toString().compareTo(o2.getKey());
+			}
+		}); 
+		if(masters.isEmpty())
+			return hypernym_overlap(features);
+		if(masters.get(0).getKey().equals("phone"))
+			return masters.get(1).getKey();
+		return masters.get(0).getKey();
+	}
+	public static String hypernym_overlap(HashSet<String> features){
 		double[] overlap_vec=new double[dim];
-		for(String f:Features){
+		for(String f:features){
 			int id=get_id(f);
 			for(int k=0;k<dim;k++){
 				if(context[id][k]>0)
@@ -309,12 +423,12 @@ public class Run {
 			}
 		}
 		for(int k=0;k<dim;k++){
-			if(overlap_vec[k]<(Features.size()*cover))//||clust_labels.containsValue(all_features.get(k)))
+			if(overlap_vec[k]<(features.size()*cover))//||clust_labels.containsValue(all_features.get(k)))
 				overlap_vec[k]=0;
 			else
 				overlap_vec[k]=1;
 		}
-		for(String f:Features){
+		for(String f:features){
 			int id=get_id(f);
 			for(int k=0;k<dim;k++){
 				if(context[id][k]>0&&overlap_vec[k]>0)
@@ -332,6 +446,7 @@ public class Run {
 				s=s+all_features.get(k-1)+" ";
 			}
 		}
+		s=s.substring(0, s.length()-1);
 		/*s=s+"="+String.valueOf(max_value-1)+" ";
 		for(int k=1;k<dim;k++){
 			if(overlap_vec[k]>0){
@@ -341,11 +456,166 @@ public class Run {
 		
 		return s;
 	}
-	
+
+	public static void prun_clust(HashSet<String> features,String hypernym){
+		List<String> to_remove=new ArrayList<String>();
+		for(String s:features){
+			Feature f=FEATURE.get(s);
+			HashSet<String> slave=new HashSet<String>();
+			HashSet<String> master=new HashSet<String>();
+			
+			slave.addAll(f.slave_feature_union.keySet());
+			master.addAll(f.master_feature_union.keySet());
+			
+//			slave.addAll(f.slave_feature_intsec.keySet());
+//			master.addAll(f.master_feature_intsec.keySet());
+			
+//			slave.addAll(f.slave_feature_comp.keySet());
+//			master.addAll(f.master_feature_comp.keySet());
+			
+//			slave.addAll(f.slave_feature_of.keySet());
+//			master.addAll(f.master_feature_of.keySet());
+			String[] hyp=hypernym.split(" ");
+			if(/*slave.contains(hyp[0])||*/master.contains(hyp[0].toString()))
+				continue;
+			slave.retainAll(features);
+			if(!slave.isEmpty())
+				continue;
+			master.retainAll(features);
+			if(!master.isEmpty())
+				continue;
+			to_remove.add(s);
+		}
+		for(String s:to_remove)
+			features.remove(s);
+		
+	}
+
+	public static String hypernym_zscore(HashSet<String> features){
+		compute_zscore(features);
+		double max_value=Double.NEGATIVE_INFINITY;
+		String hyp = null;
+		Iterator<String> it=features.iterator();
+		while(it.hasNext()){
+			String s=it.next();
+			if(fracfun(s)>max_value){
+				max_value=fracfun(s);
+				hyp=s;
+			}
+		}
+		return hyp;
+	}
+	public static double fracfun(String s){
+		return IZscore.get(s);
+//		return (1-EZscore.get(s));
+//		return (IZscore.get(s)-EZscore.get(s));
+//		return (IZscore.get(s)/EZscore.get(s));
+	}
+	public static void compute_zscore(HashSet<String> features){
+		IZscore.clear();
+		EZscore.clear();
+		compute_IS_ES(features);
+		double ui=0,ue=0;
+		int n=0;
+		for(String f:features){
+			ui+=InSim.get(f);
+			ue+=ExSim.get(f);
+			n++;
+		}
+		ui=ui/n;
+		ue=ue/n;
+		double sgmi=0,sgme=0;
+		for(String f:features){
+			sgmi+=(InSim.get(f)-ui)*(InSim.get(f)-ui);
+			sgme+=(ExSim.get(f)-ue)*(ExSim.get(f)-ue);
+		}
+		sgmi=Math.sqrt(sgmi/n);
+		sgme=Math.sqrt(sgme/n);
+		for(String f:features){
+			IZscore.put(f, (InSim.get(f)-ui)/sgmi);
+			EZscore.put(f,(ExSim.get(f)-ue)/sgme);
+		}
+//		Iterator<Entry<Integer,HashSet<String>>> iter=cid_feature.entrySet().iterator();
+//		while(iter.hasNext()){
+//			Entry<Integer,HashSet<String>> ent=iter.next();
+//			double ui=0,ue=0;
+//			int n=0;
+//			for(String f:ent.getValue()){
+//				ui+=InSim.get(f);
+//				ue+=ExSim.get(f);
+//				n++;
+//			}
+//			ui=ui/n;
+//			ue=ue/n;
+//			double sgmi=0,sgme=0;
+//			for(String f:ent.getValue()){
+//				sgmi+=(InSim.get(f)-ui)*(InSim.get(f)-ui);
+//				sgme+=(ExSim.get(f)-ue)*(ExSim.get(f)-ue);
+//			}
+//			sgmi=Math.sqrt(sgmi/n);
+//			sgme=Math.sqrt(sgme/n);
+//			for(String f:ent.getValue()){
+//				IZscore.put(f, (InSim.get(f)-ui)/sgmi);
+//				EZscore.put(f,(ExSim.get(f)-ue)/sgme);
+//			}
+//		}
+	}
+	public static void compute_IS_ES(HashSet<String> features){
+		InSim.clear();
+		ExSim.clear();
+		HashSet<String> exfeatures=new HashSet<String>(all_features);
+		exfeatures.removeAll(features);
+		for(String f:features){
+			double IS=compute_S(features,f);
+			InSim.put(f,IS);
+			double ES=compute_S(exfeatures,f);
+			ExSim.put(f,ES);
+		}
+//		Iterator<Entry<Integer,HashSet<String>>> iter=cid_feature.entrySet().iterator();
+//		while(iter.hasNext()){
+//			Entry<Integer,HashSet<String>> ent=iter.next();
+//			for(String f:ent.getValue()){
+//				double IS=compute_S(ent.getValue(),f);
+//				InSim.put(f, IS);
+//			}
+//		}
+//		
+//		HashSet<String> union=new HashSet<String>();
+//		iter=cid_feature.entrySet().iterator();
+//		while(iter.hasNext())
+//			union.addAll(iter.next().getValue());
+//		
+//		iter=cid_feature.entrySet().iterator();
+//		while(iter.hasNext()){
+//			Entry<Integer,HashSet<String>> ent=iter.next();
+//			HashSet<String> ex=new HashSet<String>();
+//			ex.addAll(union);
+//			ex.removeAll(ent.getValue());
+//			for(String f:ent.getValue()){
+//				double ES=compute_S(ex,f);
+//				ExSim.put(f, ES);
+//			}
+//		}
+		
+	}
+	public static double compute_S(HashSet<String> features,String f){
+		int id1=get_id(f);
+		double S=0;
+		int n=0;
+		for(String f2:features){
+			/*if(f.equals(f2))
+				continue;*/
+			int id2=get_id(f2);
+			S+=compute_sim(context[id1],context[id2]);
+			n++;
+		}
+		S=S/n;
+		return S;
+	}
 	
  	public static void read_features(){
 		try{
-			FileReader fr=new FileReader(folder+"/Row_labels.txt");
+			FileReader fr=new FileReader(folder+"Row_labels.txt");
 			BufferedReader bufr=new BufferedReader(fr);
 			String s;
 			while((s=bufr.readLine())!=null)
@@ -356,6 +626,26 @@ public class Run {
 		catch(Exception e){
 			e.printStackTrace();
 		}
+		
+		try{
+			File file=new File(folder+"ALL_FEATURE.dat");
+			FileInputStream fi=new FileInputStream(file);
+			ObjectInputStream oi=new ObjectInputStream(fi);
+			try{
+				while(true){
+					Feature f=(Feature) oi.readObject();
+					FEATURE.put(f.feature, f);
+				}
+			}
+			catch(EOFException e){
+				oi.close();
+				fi.close();
+			}
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		
 	}
     public static void printList(List<String> list){
         for (String string : list) {
